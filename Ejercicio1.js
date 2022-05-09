41490d79ef0231438a49b13c0edb005003c20cd2
rebegeapx <rebeca.genaro.apolinar.2000@gmail.com>
2022-05-09 11:28:35 -0500

    Actividad 5 completa

---
+++ Ejercicio1.js
@@ -0,0 +1,9 @@
+var arr=[1,4,6,10,22,55,46,2,5,0]
+
+for(let i=0; i<arr.length; i++)
+{
+    if(arr[i]>3)
+    {
+        console.log(arr[i])
+    }
+}
---
+++ Ejercicio2.js
@@ -0,0 +1,7 @@
+var arr = [];
+var con = 1;
+while(con<=5){
+    arr.push(con);
+    con++;
+}
+document.write(arr+", ");
